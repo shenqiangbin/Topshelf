024007e6824f222bad917cd5a23a833372a6e876
sqb <shenqiangbin@163.com>
2017-06-05 15:18:35 +0800

    添加脚本

---
+++ Demo01/脚本/卸载.bat
@@ -0,0 +1,16 @@
+@echo off
+
+cd %~dp0
+cd ..
+
+call demo01.exe uninstall
+
+echo; 
+
+echo Íê³É
+
+echo; 
+echo; 
+
+
+pause
---
+++ Demo01/脚本/安装.bat
@@ -0,0 +1,16 @@
+@echo off
+
+cd %~dp0
+cd ..
+
+call demo01.exe install
+
+echo; 
+
+echo Íê³É
+
+echo; 
+echo; 
+
+
+pause
