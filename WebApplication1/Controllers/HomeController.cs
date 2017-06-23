3ff0a55cb642cd49a1b9dbe408f27e5b5a52fc52
sqb <shenqiangbin@163.com>
2017-06-23 17:31:46 +0800

    add

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            //貌似web的log4net的配置文件得到bin目录的外面
+            Base.SqlLogger.Log("mvc test");
+
+            return Content("ok");
+        }
+    }
+}
